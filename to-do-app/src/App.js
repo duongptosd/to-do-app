ed2167287f433ed09307c8ff03340b7b09865e07
Duong Pham <duong.pham@orientsoftware.com>
2019-10-29 17:31:18 +0700

    loading page

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Pane, Dialog } from 'evergreen-ui';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -10,9 +10,20 @@ import LogIn from './components/log-in';
 function App() {
   const [state, setState] = useState({
     isShown: false,
-    isLoading: false
+    isLoading: true
   });
+
+  const fakeAsync = () => new Promise((resolve) => setTimeout(() => resolve(), 300000));
+
+  useEffect(async () => {
+    await fakeAsync();
+    setState(
+      { isLoading: false }
+    )
+  }, []);
+
   return (
+    state.isLoading ? <LoadingSpinner /> :
     <Router>
       <Switch>
         <Route path='/login'>
@@ -48,6 +59,29 @@ function App() {
         </Route>
       </Switch>
     </Router>
+  );
+}
+
+const LoadingSpinner = () => {
+  return (
+    <svg class="loading-spinner" height="100%" width="100%" viewBox="0 0 100 100">
+        <rect className="cube cube1" x={48} y={48} width={1} height={1} />
+        <rect className="cube cube2" x={49} y={48} width={1} height={1} />
+        <rect className="cube cube3" x={50} y={48} width={1} height={1} />
+        <rect className="cube cube4" x={51} y={48} width={1} height={1} />
+        <rect className="cube cube5" x={48} y={49} width={1} height={1} />
+        <rect className="cube cube6" x={49} y={49} width={1} height={1} />
+        <rect className="cube cube7" x={50} y={49} width={1} height={1} />
+        <rect className="cube cube8" x={51} y={49} width={1} height={1} />
+        <rect className="cube cube9" x={48} y={50} width={1} height={1} />
+        <rect className="cube cube10" x={49} y={50} width={1} height={1} />
+        <rect className="cube cube11" x={50} y={50} width={1} height={1} />
+        <rect className="cube cube12" x={51} y={50} width={1} height={1} />
+        <rect className="cube cube13" x={48} y={51} width={1} height={1} />
+        <rect className="cube cube14" x={49} y={51} width={1} height={1} />
+        <rect className="cube cube15" x={50} y={51} width={1} height={1} />
+        <rect className="cube cube16" x={51} y={51} width={1} height={1} />
+    </svg>
   );
 }
 
