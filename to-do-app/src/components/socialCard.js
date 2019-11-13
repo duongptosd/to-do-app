b9b51ad186bf3c2158cf8aefc2951e0c47b58da3
Duong Pham <duong.pham@orientsoftware.com>
2019-11-13 17:17:08 +0700

    fix card z-index

--- to-do-app/src/components/socialCard.js
+++ to-do-app/src/components/socialCard.js
@@ -1,57 +1,56 @@
 import React from 'react';
-import { Pane, Button } from 'evergreen-ui';
 import '../styles/socialCard.css'
 import 'font-awesome/css/font-awesome.min.css';
 
 export const SocialCard = () => {
     return (
-        <Pane className="card-wrapper">
-            <Pane className="card">
-                <Pane className="top">
-                    <Pane className="social-buttons">
+        <div className="card-wrapper">
+            <div className="card">
+                <div className="top">
+                    <div className="social-buttons">
                         <button>
                             <i className="fa fa-camera"></i>
                         </button>
                         <button>
-                            <i className="fa fa-camera"></i>
-                        </button>
-                        <button>
-                            <i className="fa fa-camera"></i>
-                        </button>
-                    </Pane>
-                    <Pane className="text">
-                        <Pane className="wrapper-name">
-                            <Pane className="name">Morgan, 22</Pane>
                             <i className="fa fa-heart"></i>
-                        </Pane>
-                        <Pane className="title">DotA Player</Pane>
-                    </Pane>
-                    <button className="like">
-                        <Pane className="hexagon">
+                        </button>
+                        <button>
                             <i className="fa fa-user"></i>
-                        </Pane>
+                        </button>
+                    </div>
+                    <div className="text">
+                        <div className="wrapper-name">
+                            <div className="name">Dendi, 29</div>
+                            <i className="fa fa-check-circle"></i>
+                        </div>
+                        <div className="title">Dota 2 Player</div>
+                    </div>
+                    <button className="like">
+                        <div className="hexagon">
+                            <i className="fa fa-heart"></i>
+                        </div>
                     </button>
-                </Pane>
-                <Pane className="bottom">
-                    <Pane className="desc">
-                        What does a hero truly need? Anti-mage needs Battle Fury, Axe needs Blink Dagger, while Daedalus is the core item for Kunkka.
-                    </Pane>
-                    <Pane className="buttons">
+                </div>
+                <div className="bottom">
+                    <div className="desc">
+                    Danil "Dendi" Ishutin (born December 30, 1989) is a Ukrainian professional Dota 2 and former DotA player. Well known for his solo mid skills, he is regarded as one of the most creative players in the scene today.
+                    </div>
+                    <div className="buttons">
                         <button className="height">
                             <i className="fa fa-user"></i>
-                            <Pane>5'4</Pane>
+                            <div className="personal-info">5'4</div>
                         </button>
                         <button className="marriage">
                             <i className="fa fa-user"></i>
-                            <Pane>Married</Pane>
+                            <div className="personal-info">Married</div>
                         </button>
                         <button className="zodiac-sign">
                             <i className="fa fa-user"></i>
-                            <Pane>Gemini</Pane>
+                            <div className="personal-info">Gemini</div>
                         </button>
-                    </Pane>
-                </Pane>
-            </Pane>
-        </Pane>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
